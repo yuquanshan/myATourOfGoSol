31430182247c249945fc0dd35f6f1113f8742a6f
yuquanshan <springshan13@gmail.com>
2016-07-12 00:56:11 -0400

    Create readers.go

---
+++ readers.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (r MyReader)Read(b []byte)(int, error){
+	for i := 0; i<len(b);i++{
+		b[i] = byte('A')
+	}
+	return len(b), nil
+}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+
+func main() {
+	reader.Validate(MyReader{})
+}
